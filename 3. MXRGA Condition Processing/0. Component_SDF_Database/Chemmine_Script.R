7e9fa87cb8947568a2afab644128c00e9221c2f9
jujuisland <105098943+KJ-Liao@users.noreply.github.com>
2024-04-01 15:18:25 +0800

    Update Chemmine_Script.R

--- 3. MXRGA Condition Processing/0. Component_SDF_Database/Chemmine_Script.R
+++ 3. MXRGA Condition Processing/0. Component_SDF_Database/Chemmine_Script.R
@@ -3,29 +3,6 @@ sdf_database <- read.SDFset("MERGE_SDF.txt")
 valid <- validSDF(sdf_database)
 sdfset <- sdf_database[valid]
 
-# MCS Tanimoto
-library(fmcsR)
-
-# Empty Matrix
-DISM <- c() 
-
-for(i in c(1:length(sdfset))){
-  Dists <- c()
-  for(j in c(1:length(sdfset))){
-    
-    MCS <-fmcs(sdfset[i], sdfset[j], au=0, bu=0)
-    result <- unname(MCS@stats[4])
-    Dists <- c(Dists, result) 
-  }
-  DISM <- rbind(DISM, Dists) 
-}
-
-write.csv(DISM, "MCS_DISM.csv")
-
-#
-#
-#
-
 # AP Tanimoto
 apset <- sdf2ap(sdfset)
 
