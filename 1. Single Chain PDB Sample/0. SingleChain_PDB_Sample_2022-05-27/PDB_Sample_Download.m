a0707cf6ce6eb023523b5ed81bc24b11e27e2c80
jujuisland <105098943+KJ-Liao@users.noreply.github.com>
2024-04-01 14:17:44 +0800

    Create PDB_Sample_Download.m

---
+++ 1. Single Chain PDB Sample/0. SingleChain_PDB_Sample_2022-05-27/PDB_Sample_Download.m
@@ -0,0 +1,23 @@
+%%%% Load ID of PDB Sample
+Filename='Accessible_SingleChain_Human_Sample.txt';
+
+%%%% Build up List of PDB Sample ID
+File=fopen(Filename);
+Sample(1).ID=[];m=1;
+line=fgetl(File);
+while (1)
+    line=fgetl(File);
+    if line==-1, break, end 
+    Sample(m).ID=sscanf(line,'%c');
+    m=m+1;         
+end
+fclose(File);
+
+%%%% Download Files of PDB Sample from PDB Web
+% Wrong or Changed(Renamed) PDB ID lead to 0 KB html logs
+% If So, Please Correct the PDB ID or Remove them from Input_Sample.txt
+for n=1:length(Sample)   
+    URL=strcat('https://files.rcsb.org/download/',lower(Sample(n).ID),'.pdb');
+    websave(lower(Sample(n).ID),URL);
+    Reamined_No=length(Sample)-n
+end
