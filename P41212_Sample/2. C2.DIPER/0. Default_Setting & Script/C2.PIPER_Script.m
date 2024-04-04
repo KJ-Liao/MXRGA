08899bb7162de03398f7d8fe893e35cb0e2d93da
jujuisland <105098943+KJ-Liao@users.noreply.github.com>
2024-04-04 15:09:12 +0800

    Add files via upload

---
+++ P41212_Sample/2. C2.DIPER/0. Default_Setting & Script/C2.PIPER_Script.m
@@ -0,0 +1,47 @@
+%%%% Create the Directory Pathes
+% Load Input_Sample.txt
+File=fopen('Input_Sample.txt');
+
+% Create the Deposited File
+s=1;
+Sample_Info(1).ID=[];
+while (1)
+    line=fgetl(File);
+    if line==-1, break, end 
+    Sample_Info(s).ID=sscanf(line,'%c');
+    mkdir(lower(Sample_Info(s).ID(1:4)));
+    s=s+1;         
+end
+fclose(File);
+
+%
+%
+%
+
+%%%% Sample Preparation: Sample_prep.txt
+Script_Prep = fopen([pwd,'/Sample_prep.txt'],'w');
+fprintf(Script_Prep,'#!/bin/bash');
+fprintf(Script_Prep,'\n');
+for r=1:size(Sample_Info,2)
+    fprintf(Script_Prep,'\n');
+    fprintf(Script_Prep,['../protein_prep/prepare.py',' ',lower(Sample_Info(r).ID(1:4)),'_trun_AF.pdb']);   
+end  
+fclose(Script_Prep);
+
+%
+%
+%
+
+%%%% Script fot PIPER: Sample_PIPER.txt
+% Directory Path of "piper_package"
+Path='/home/sunmd33452/Desktop/piper_package/PIPER_Sample/';
+
+% Write Sample_PIPER.txt
+Script_PIPER = fopen([pwd,'/Sample_PIPER.txt'],'w');
+fprintf(Script_PIPER,'#!/bin/bash');
+fprintf(Script_PIPER,'\n');
+for r=1:size(Sample_Info,2)
+    fprintf(Script_PIPER,'\n');
+    fprintf(Script_PIPER,['../piper -vv -c1.0 -k4 --msur_k=1.0 --maskr=1.0 -T FFTW_EXHAUSTIVE -R 3281 -t 1 -p ../prms/atoms.prm -f ../prms/Coeff_DIPER.prm -r ../prms/C2_rots.prm',' ',lower(Sample_Info(r).ID(1:4)),'_trun_AF_pnon.pdb',' ',lower(Sample_Info(r).ID(1:4)),'_trun_AF_pnon.pdb',' ','--o ', Path,lower(Sample_Info(r).ID(1:4)),'/']);
+end  
+fclose(Script_PIPER);
