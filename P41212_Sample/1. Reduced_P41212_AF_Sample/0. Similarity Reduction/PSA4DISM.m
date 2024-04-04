f7115b41c120804fef68089fdbe72b3e6031cf7a
jujuisland <105098943+KJ-Liao@users.noreply.github.com>
2024-04-04 14:00:46 +0800

    Add files via upload

---
+++ P41212_Sample/1. Reduced_P41212_AF_Sample/0. Similarity Reduction/PSA4DISM.m
@@ -0,0 +1,8 @@
+%%%% Pairwise sequence alignment matix (DISM) for Rplot 
+% Unadjusted dissimilarity as distance
+Data=table2struct(readtable('P41212_Sample.xlsx'));
+D=seqpdist({Data(1:size(Data,1)).Seq},'Method', 'p-distance');
+
+% Output
+Output=[{'PDB_ID'},{Data.ID}; {Data.ID}',num2cell(squareform(D))];
+writecell(Output,'DISM.xlsx');
