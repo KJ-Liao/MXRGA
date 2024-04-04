e1a9cc837fe27435ffe43acb2d7e990fd18ea75f
jujuisland <105098943+KJ-Liao@users.noreply.github.com>
2024-04-04 14:16:32 +0800

    Add files via upload

---
+++ P41212_Sample/1. Reduced_P41212_AF_Sample/1. Reduced_PDB_Sample/PDB_Sample_Download.m
@@ -0,0 +1,27 @@
+%%%% Load ID of PDB Sample
+% PDB ID line by line (if there were multiple samples)
+% Example:
+% 1B42
+% 1C46
+% ...
+Filename='Input_Sample.txt';
+
+%%%% Build up List of PDB Sample ID
+File=fopen(Filename);
+Sample(1).ID=[];m=1;
+while (1)
+    line=fgetl(File);
+    if line==-1, break, end 
+    Sample(m).ID=sscanf(line,'%c');
+    m=m+1;         
+end
+fclose(File);
+
+%%%% Download Files of PDB Sample from PDB Web
+% Wrong or Changed(Renamed) PDB ID lead to 0 KB html logs
+% If So, Please Correct the PDB ID or Remove them from Input_Sample.txt
+for n=1:length(Sample)   
+    URL=strcat('https://files.rcsb.org/download/',lower(Sample(n).ID),'.pdb');
+    websave(lower(Sample(n).ID),URL);
+    Reamined_No=length(Sample)-n
+end
